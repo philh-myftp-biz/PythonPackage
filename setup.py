53801008dead5b9a245ad9d92c2e7a618ce4f6e2
Phil Hunt <philiphuntiv2@gmail.com>
2026-09-11 14:03:40 -0400

    Skip compilation if nothing changed

--- setup.py
+++ setup.py
@@ -8,24 +8,37 @@ root = Path(__file__).parent.resolve()
 
 pybind = Path(gettempdir()) / "philh_myftp_biz-pybind"
 
-run([
-    "git.exe", "clone",
-    "--depth", "1",
-    "https://github.com/MineFartS/pybind",
-    str(pybind)
-])
+mtime = lambda p: p.stat().st_mtime
+
+pydfiles: list[tuple[Path, Path]] = []
+
+for src in (root / "philh_myftp_biz").rglob("*.cpp"):
+
+    dst = src.with_suffix(".pyd")
+
+    if dst.exists():
+        
+        _cfiles: list[Path] = [src, *src.parent.rglob("*.h"), *src.parent.rglob("*.hpp")]
+        
+        if any(mtime(h) > mtime(dst) for h in _cfiles):
+            continue
+
+    pydfiles += [(src, dst)]
+
+if len(pydfiles) > 0:
+    run([
+        "git", "clone",
+        "--depth", "1",
+        "https://github.com/MineFartS/pybind",
+        str(pybind)
+    ])
 
 compile_command = [
     'Powershell.exe', 
     '-File', f'{pybind}/build.ps1'
 ]
 
-for src in (root / "philh_myftp_biz").rglob("*.cpp"):
-
-    dst = src.with_suffix(".pyd")
-
-    print(f'Compiling: {src} -> {dst}')
-
+for src, dst in pydfiles:
     run(
         args = [
             *compile_command,
