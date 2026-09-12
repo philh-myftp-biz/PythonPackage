674693dd8a612cabe5b6ee29b7c96615ba617d79
Phil Hunt <philiphuntiv2@gmail.com>
2026-09-12 07:35:07 -0400

    install.bat: Add -f arg

--- install.bat
+++ install.bat
@@ -2,12 +2,23 @@
 cls
 cd /d "%~dp0"
 
-:: Install/Update Package
-pip install --upgrade %* . || exit /b 1
+if "%~1"=="-f" (
+    
+    rmdir /s /q "philh_myftp_biz.egg-info"
+    rmdir /s /q "build"
+    rmdir /s /q "dist"
+    
+    pip uninstall philh_myftp_biz -y
+    
+    pip cache purge
+
+)
+
+pip install . || exit /b 1
 
 :: Locate package directory using pip
 for /f "delims=" %%i in ('pip show philh_myftp_biz ^| findstr "Location:"') do set "DIR=%%i"
-Set "DIR=%DIR:~10%\philh_myftp_biz"
+set "DIR=%DIR:~10%\philh_myftp_biz"
 
 :: Precompile package
 python -m compileall -f "%DIR%"
