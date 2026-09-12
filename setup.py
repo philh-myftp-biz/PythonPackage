d2559436369ee278baa2b9207cbdfada39ca3f83
Phil Hunt <philiphuntiv2@gmail.com>
2026-09-12 08:26:21 -0400

    Generate pyi files on build

--- setup.py
+++ setup.py
@@ -1,18 +1,35 @@
+from setuptools.command.build_ext import build_ext
 from pybind11 import get_include
-from pathlib import Path
+from os.path import abspath
 from sys import platform
+from pathlib import Path
+from mypy import stubgen
 import setuptools as st
+import sys
+
+class BuildExtWithStubs(build_ext):
+    def run(self) -> None:
+        
+        self.run_command("build_py")        
+        super().run()
+
+        sys.path.insert(0, abspath(self.build_lib))
+
+        for ext in self.extensions:
+            stubgen.generate_stubs(stubgen.parse_options([
+                '-m', ext.name,
+                '-o', abspath(self.build_lib),
+                '--inspect-mode'
+            ]))
 
 kw = {
     'name': "philh_myftp_biz",
     'packages': st.find_namespace_packages(exclude=["build*", "dist*", "tests*", "docs*"]),
-    'ext_modules': []
+    'ext_modules': [],
+    'cmdclass': {
+        'build_ext': BuildExtWithStubs
+    },
 }
-
-if platform == "win32":
-    extra_compile_args = ["/std:c++20", "/EHsc"]
-else:
-    extra_compile_args = ["-std=c++20"]
 
 for cpp in Path(kw['name']).rglob("*.cpp"):
 
@@ -23,7 +40,7 @@ for cpp in Path(kw['name']).rglob("*.cpp"):
             cpp.parent.as_posix(),
             get_include()
         ],
-        extra_compile_args = extra_compile_args,
+        extra_compile_args = (["/std:c++20", "/EHsc"] if platform=="win32" else ["-std=c++20"]),
         language = 'c++'
     )]
 
