da03fb25f273eec8c14769d0aaa0d5667b5e27f4
Phil Hunt <philiphuntiv2@gmail.com>
2026-09-13 08:07:36 -0400

    Revert "install.bat: Accept -e editor mode"
    This reverts commit 8251ea8c60b2f89b17a9fbd5f96e2e8a64fcfb93.

--- install.bat
+++ install.bat
@@ -2,37 +2,23 @@
 cls
 cd /d "%~dp0"
 
-set "f=0"
-set "e=0"
-
-:parse_args
-if "%~1"=="" goto end_parse
-if /I "%~1"=="-f" set "f=1"
-if /I "%~1"=="-e" set "e=1"
-shift
-goto parse_args
-:end_parse
-
-if "%f%"=="1" (
+if "%~1"=="-f" (
     
     rmdir /s /q "philh_myftp_biz.egg-info"
     rmdir /s /q "build"
     rmdir /s /q "dist"
-    del /q "philh_myftp_biz\*.pyd"
     
     pip uninstall philh_myftp_biz -y
+    
     pip cache purge
 
 )
 
-if "%e%"=="1" (
-    pip install -e .
-) else (
-    pip install .
-)
-IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+pip install . || exit /b 1
 
+:: Locate package directory using pip
 for /f "delims=" %%i in ('pip show philh_myftp_biz ^| findstr "Location:"') do set "DIR=%%i"
 set "DIR=%DIR:~10%\philh_myftp_biz"
 
+:: Precompile package
 python -m compileall -f "%DIR%"
