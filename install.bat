8251ea8c60b2f89b17a9fbd5f96e2e8a64fcfb93
Phil Hunt <philiphuntiv2@gmail.com>
2026-09-13 06:49:59 -0400

    install.bat: Accept -e editor mode

--- install.bat
+++ install.bat
@@ -2,23 +2,37 @@
 cls
 cd /d "%~dp0"
 
-if "%~1"=="-f" (
+set "f=0"
+set "e=0"
+
+:parse_args
+if "%~1"=="" goto end_parse
+if /I "%~1"=="-f" set "f=1"
+if /I "%~1"=="-e" set "e=1"
+shift
+goto parse_args
+:end_parse
+
+if "%f%"=="1" (
     
     rmdir /s /q "philh_myftp_biz.egg-info"
     rmdir /s /q "build"
     rmdir /s /q "dist"
+    del /q "philh_myftp_biz\*.pyd"
     
     pip uninstall philh_myftp_biz -y
-    
     pip cache purge
 
 )
 
-pip install . || exit /b 1
+if "%e%"=="1" (
+    pip install -e .
+) else (
+    pip install .
+)
+IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
-:: Locate package directory using pip
 for /f "delims=" %%i in ('pip show philh_myftp_biz ^| findstr "Location:"') do set "DIR=%%i"
 set "DIR=%DIR:~10%\philh_myftp_biz"
 
-:: Precompile package
 python -m compileall -f "%DIR%"
