e7964d6c422118af1d3cc851c6e9fdbd7c74f523
Phil Hunt <philiphuntiv2@gmail.com>
2026-09-14 12:33:16 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,21 @@ sudo apt update
 sudo apt install -y python3
 sudo apt install -y python3-pip
 
-python3 -m pip install . --break-system-packages --ignore-installed urllib3
+clear
+
+if [ "$1" == "-f" ]; then
+
+    rm -rf "philh_myftp_biz.egg-info"
+    rm -rf "build"
+    rm -rf "dist"
+
+    python3 -m pip uninstall -y philh_myftp_biz \
+        --break-system-packages
+    
+    pip cache purge
+
+fi
+
+python3 -m pip install -vvv . \
+    --break-system-packages
 
