48adf06b96b4fe07d6c10e297a1203cf657fb537
Phil Hunt <philiphuntiv2@gmail.com>
2026-09-16 04:52:22 -0400

    Only compile hardware devices for windows

--- philh_myftp_biz/pc/hardware.cpp
+++ philh_myftp_biz/pc/hardware.cpp
@@ -3,12 +3,13 @@
 #include <string>
 #include <remap.h>
 
-#include <_hw/hdd.hpp>
-#include <_hw/pcie.hpp>
-#include <_hw/vdisk.hpp>
 #include <_hw/device.h>
 
-namespace py = pybind11;
+#ifdef WINDOWS
+    #include <_hw/hdd.hpp>
+    #include <_hw/pcie.hpp>
+    #include <_hw/vdisk.hpp>
+#endif
 
 PYBIND11_MODULE(hardware, m) {
 
@@ -18,39 +19,42 @@ PYBIND11_MODULE(hardware, m) {
         .def_property_readonly("Name", &Device::GetName)
         .def_property_readonly("Connected", &Device::GetConnected);
 
-    // Bind HardDrive as a child of Device
-    py::class_<HardDrive, Device>(m, "HardDrive")
-        .def(py::init<str, str, int, str>(),
-            py::arg("Tower"), 
-            py::arg("Conn"), 
-            py::arg("ID"), 
-            py::arg("SN")
-        )
-        .def_readonly("Tower", &HardDrive::Tower)
-        .def_readonly("Conn", &HardDrive::Conn)
-        .def_readonly("ID", &HardDrive::ID)
-        .def_readonly("SN", &HardDrive::SN)
-        .def_readonly("Index", &HardDrive::Index)      
-        .def_property("FriendlyName", &HardDrive::FriendlyName, &HardDrive::setFriendlyName)
-        .def_property("Usage", &HardDrive::Usage, &HardDrive::setUsage);
+    #ifdef WINDOWS
+    
+        // Bind HardDrive as a child of Device
+        py::class_<HardDrive, Device>(m, "HardDrive")
+            .def(py::init<str, str, int, str>(),
+                py::arg("Tower"), 
+                py::arg("Conn"), 
+                py::arg("ID"), 
+                py::arg("SN")
+            )
+            .def_readonly("Tower", &HardDrive::Tower)
+            .def_readonly("Conn", &HardDrive::Conn)
+            .def_readonly("ID", &HardDrive::ID)
+            .def_readonly("SN", &HardDrive::SN)
+            .def_readonly("Index", &HardDrive::Index)
+            .def_property("FriendlyName", &HardDrive::FriendlyName, &HardDrive::setFriendlyName)
+            .def_property("Usage", &HardDrive::Usage, &HardDrive::setUsage);
 
-    // Bind PCIeCard as a child of Device
-    py::class_<PCIeCard, Device>(m, "PCIeCard")
-        .def(py::init<str, int, str>(),
-            py::arg("Slot"), py::arg("Lanes"), py::arg("DeviceId"))
-        .def_readonly("Slot", &PCIeCard::Slot)
-        .def_readonly("Lanes", &PCIeCard::Lanes)
-        .def_readonly("DeviceId", &PCIeCard::DeviceId);
+        // Bind PCIeCard as a child of Device
+        py::class_<PCIeCard, Device>(m, "PCIeCard")
+            .def(py::init<str, int, str>(),
+                py::arg("Slot"), py::arg("Lanes"), py::arg("DeviceId"))
+            .def_readonly("Slot", &PCIeCard::Slot)
+            .def_readonly("Lanes", &PCIeCard::Lanes)
+            .def_readonly("DeviceId", &PCIeCard::DeviceId);
 
-    // Bind VirtualDisk as a child of Device
-    py::class_<VirtualDisk, Device>(m, "VirtualDisk")
-        .def(py::init<str, str>(),
-            py::arg("Name"), 
-            py::arg("Mount")
-        )
-        .def_readonly("Mount", &VirtualDisk::Mount)
-        .def_property("Connected", &VirtualDisk::GetConnected, &VirtualDisk::setConnected)
-        .def("Repair", &VirtualDisk::Repair);
+        // Bind VirtualDisk as a child of Device
+        py::class_<VirtualDisk, Device>(m, "VirtualDisk")
+            .def(py::init<str, str>(),
+                py::arg("Name"), 
+                py::arg("Mount")
+            )
+            .def_readonly("Mount", &VirtualDisk::Mount)
+            .def_property("Connected", &VirtualDisk::GetConnected, &VirtualDisk::setConnected)
+            .def("Repair", &VirtualDisk::Repair);
 
+    #endif
 }
 
