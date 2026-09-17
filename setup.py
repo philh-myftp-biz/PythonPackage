1ba732b6d7ada0a74eeea1083a50f91e849e72b0
Phil Hunt <philiphuntiv2@gmail.com>
2026-09-17 09:29:37 -0400

    Manually add exts to setup.py

--- setup.py
+++ setup.py
@@ -1,66 +1,20 @@
-from setuptools.command.build_ext import build_ext
-from os.path import abspath, exists
-from subprocess import check_call
-from pybind11 import get_include
-from sys import platform
-from pathlib import Path
-from mypy import stubgen
-import setuptools as st
-import sys
+from pybind_setup_ext import cpp_ext, setup, update_submodule
 
-class BuildExtWithStubs(build_ext):
+update_submodule('headers', force=True, remote=True)
 
-    @staticmethod
-    def _gen_stubs(m:str, o:str):
-        stubgen.generate_stubs(stubgen.parse_options([
-            '-m', m,
-            '-o', abspath(o), 
-            '--inspect-mode'
-        ]))
-
-    def run(self) -> None:
-        
-        super().run()
-
-        sys.path.insert(0, abspath(self.build_lib))
-
-        for ext in self.extensions:
-
-            self._gen_stubs(m=ext.name, o=self.build_lib)
-
-            self._gen_stubs(m=ext.name, o='.')
-
-if not exists("headers/README.md"):
-    check_call(['git', 'submodule', 'update', '--init'])
-
-ext_modules = []
-
-ext_kw = {
-    'extra_compile_args': ["-fvisibility=hidden"],
-    'language': 'c++',
+kw = {
+    'include_dirs': ["headers"]
 }
 
-if platform == "win32":
-    ext_kw['extra_compile_args'] += ["/std:c++20", "/EHsc"]
-else:
-    ext_kw['extra_compile_args'] += ["-std=c++20"]
-
-include_dirs = [
-    get_include(),
-    "headers"
-]
-
-for cpp in Path("philh_myftp_biz").rglob("*.cpp"):
-
-    ext_modules += [st.Extension(
-        name = cpp.as_posix().rsplit('.', 1)[0].replace('/', '.'),
-        sources = [cpp.as_posix()],
-        include_dirs = [*include_dirs, cpp.parent.as_posix()],
-        **ext_kw
-    )]
-
-st.setup(
-    ext_modules = ext_modules,
-    cmdclass = {'build_ext': BuildExtWithStubs}
+setup(
+    cpp_ext("philh_myftp_biz/_log.cpp", **kw),
+    cpp_ext("philh_myftp_biz/num.cpp", **kw),
+    cpp_ext("philh_myftp_biz/web/_web.cpp", **kw),
+    cpp_ext("philh_myftp_biz/time/_time.cpp", **kw),
+    cpp_ext("philh_myftp_biz/text/uio.cpp", **kw),
+    cpp_ext("philh_myftp_biz/text/hex.cpp", **kw),
+    cpp_ext("philh_myftp_biz/text/contains.cpp", **kw),
+    cpp_ext("philh_myftp_biz/pc/hardware.cpp", **kw),
+    cpp_ext("philh_myftp_biz/pc/_pc.cpp", **kw),
 )
 
